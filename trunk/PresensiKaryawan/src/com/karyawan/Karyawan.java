07e23be1add05a95f71c24329010e817aa756748
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-09-03 13:06:06 +0000

    commit library

--- trunk/PresensiKaryawan/src/com/karyawan/Karyawan.java
+++ trunk/PresensiKaryawan/src/com/karyawan/Karyawan.java
@@ -4,10 +4,17 @@
  */
 package com.karyawan;
 
+import java.util.GregorianCalendar;
+
 /**
  *
  * @author Tinus
  */
 public class Karyawan {
-    
+    //golongan, nama, alamat, nip, pelanggan, tgl masuk, posisi
+    private String nip;
+    private String nama;
+    private String alamat;
+    private GregorianCalendar tanggal_masuk;
+   
 }
