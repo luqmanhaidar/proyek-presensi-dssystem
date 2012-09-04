23370b663e141e52b0d03e7ca1b18b30f4572ce7
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-09-04 15:39:07 +0000

    new golongan

--- PresensiKaryawan/src/com/presensikaryawan/golongan/GolonganDaoImplemen.java
+++ PresensiKaryawan/src/com/presensikaryawan/golongan/GolonganDaoImplemen.java
@@ -5,7 +5,10 @@
 package com.presensikaryawan.golongan;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,13 +17,13 @@ import java.util.List;
  */
 public class GolonganDaoImplemen implements GolonganDao{
     
-    private final String SQL_INSERT = "insert into kategori(kodeKategori,namaKategori) values (?,?)";
-    private final String SQL_UPDATE = "update kategori set namaKategori = ? where kodeKategori = ?";
-    private final String SQL_DELETE = "delete from kategori where kodeKategori like ?";
-    private final String SQL_GETBYKODE = "select * from kategori where kodeKategori like ?";
-    private final String SQL_GETBYNAMA = "select * from kategori where namaKategori like ?";
-    private final String SQL_GETALL = "select * from kategori";
-    private final String SQL_GETALLKODE = "select kodeKategori from kategori";
+    private final String SQL_INSERT = "insert into golongan(kode_golongan,nama_golongan) values (?,?)";
+    private final String SQL_UPDATE = "update golongan set nama_golongan = ? where kode_golongan = ?";
+    private final String SQL_DELETE = "delete from golongan where kode_golongan like ?";
+    private final String SQL_GETBYKODE = "select * from golongan where kode_golongan like ?";
+    private final String SQL_GETBYNAMA = "select * from golongan where nama_golongan like ?";
+    private final String SQL_GETALL = "select * from golongan";
+    private final String SQL_GETALLKODE = "select kode_golongan from golongan";
     private Connection connection;
 
     public GolonganDaoImplemen(Connection connection) {
@@ -29,32 +32,198 @@ public class GolonganDaoImplemen implements GolonganDao{
 
     @Override
     public void insert(Golongan golongan) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PreparedStatement statement = null;
+        ResultSet result = null;
+        try {
+            connection.setAutoCommit(false);
+
+            statement = connection.prepareStatement(SQL_INSERT);
+            statement.setString(1, golongan.getKodeGolongan());
+            statement.setString(2, golongan.getNamaGolongan());
+            statement.executeUpdate();
+
+            connection.commit();
+        } catch (SQLException exception) {
+            connection.rollback();
+            throw exception;
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                if (result != null) {
+                    result.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException exception) {
+                throw exception;
+            }
+        }
     }
 
     @Override
     public void update(Golongan golongan) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PreparedStatement statement = null;
+         try {
+             connection.setAutoCommit(false);
+
+             statement = connection.prepareStatement(SQL_UPDATE);
+             statement.setString(1,golongan.getNamaGolongan());
+             statement.setString(2,golongan.getKodeGolongan());
+             statement.executeUpdate();
+
+             connection.commit();
+         } catch (SQLException exception) {
+              connection.rollback();
+             throw exception;
+         } finally {
+             try {
+                 connection.setAutoCommit(true);
+                 if (statement != null) {
+                     statement.close();
+                 }
+             } catch (SQLException exception) {
+                 throw exception;
+             }
+         }
     }
 
     @Override
     public void delete(Golongan unit) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PreparedStatement statement = null;
+         try {
+             connection.setAutoCommit(false);
+
+             statement = connection.prepareStatement(SQL_DELETE);
+             statement.setString(1, unit.getKodeGolongan());
+             statement.executeUpdate();
+
+             connection.commit();
+         } catch (SQLException exception) {
+              connection.rollback();
+             throw exception;
+         } finally {
+             try {
+                 connection.setAutoCommit(true);
+                 if (statement != null) {
+                     statement.close();
+                 }
+             } catch (SQLException exception) {
+                 throw exception;
+             }
+         }
     }
 
     @Override
     public Golongan getByKodeGolongan(String kodeGolongan) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PreparedStatement statement = null;
+         ResultSet result = null;
+         try {
+             connection.setAutoCommit(false);
+             statement = connection.prepareStatement(SQL_GETBYKODE);
+
+               statement.setString(1, kodeGolongan);
+             result = statement.executeQuery();
+             Golongan golongan = null;
+             if (result.next()) {
+                 golongan = new Golongan();
+                 golongan.setKodeGolongan(result.getString("kode_golongan"));
+                 golongan.setNamaGolongan(result.getString("nama_golongan"));
+             }
+
+             connection.commit();
+             return golongan;
+         } catch (SQLException exception) {
+              connection.rollback();
+             throw exception;
+         } finally {
+             try {
+                 connection.setAutoCommit(true);
+                 if (result != null) {
+                     result.close();
+                 }
+                 if (statement != null) {
+                     statement.close();
+                 }
+             } catch (SQLException exception) {
+                 throw exception;
+             }
+         }
     }
 
     @Override
     public Golongan getByNamaGolongan(String namaGolongan) throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PreparedStatement statement = null;
+         ResultSet result = null;
+         try {
+             connection.setAutoCommit(false);
+             statement = connection.prepareStatement(SQL_GETBYNAMA);
+            
+               statement.setString(1, namaGolongan);
+               System.out.println(SQL_GETBYNAMA);
+             result = statement.executeQuery();
+             Golongan golongan = null;
+             if (result.next()) {
+                 golongan = new Golongan();
+                 golongan.setKodeGolongan(result.getString("kode_golongan"));
+                 golongan.setNamaGolongan(result.getString("nama_golongan"));
+             }
+
+             connection.commit();
+             return golongan;
+         } catch (SQLException exception) {
+              connection.rollback();
+             throw exception;
+         } finally {
+             try {
+                 connection.setAutoCommit(true);
+                 if (result != null) {
+                     result.close();
+                 }
+                 if (statement != null) {
+                     statement.close();
+                 }
+             } catch (SQLException exception) {
+                 throw exception;
+             }
+         }
     }
 
     @Override
     public List<Golongan> getAllGolongan() throws SQLException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        PreparedStatement statement = null;
+         ResultSet result = null;
+         try {
+             connection.setAutoCommit(false);
+
+             statement = connection.prepareStatement(SQL_GETALL);
+
+             result = statement.executeQuery();
+             List<Golongan> golongans = new ArrayList<Golongan>();
+             while (result.next()) {
+                 Golongan golongan = new Golongan();
+                 golongan.setKodeGolongan(result.getString("kode_golongan"));
+                 golongan.setNamaGolongan(result.getString("nama_golongan"));
+                 golongans.add(golongan);
+             }
+
+             connection.commit();
+             return golongans;
+         } catch (SQLException exception) {
+             throw exception;
+         } finally {
+             try {
+                 connection.setAutoCommit(true);
+                 if (result != null) {
+                     result.close();
+                 }
+                 if (statement != null) {
+                     statement.close();
+                 }
+             } catch (SQLException exception) {
+                 throw exception;
+            }
+         }
     }
     
 }
