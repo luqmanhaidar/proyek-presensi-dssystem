9ca17ff0b7b91882d034b5b4f172cebfc50e2a29
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-09-05 16:24:54 +0000

    libur perusahaan & nasional (revised): DONE!

--- PresensiKaryawan/src/com/presensikaryawan/liburNasional/LiburNasionalDaoImplemen.java
+++ PresensiKaryawan/src/com/presensikaryawan/liburNasional/LiburNasionalDaoImplemen.java
@@ -4,19 +4,26 @@
  */
 package com.presensikaryawan.liburNasional;
 
+import com.presensikaryawan.liburPerusahaan.LiburPerusahaanForm;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.StringTokenizer;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Eki
  */
-public class LiburNasionalDaoImplemen implements LiburNasionalDao{
-    
+public class LiburNasionalDaoImplemen implements LiburNasionalDao {
+
     private final String SQL_INSERT = "insert into libur_nasional(tanggal,keterangan) values (?,?)";
     private final String SQL_UPDATE = "update libur_nasional set keterangan = ? where tanggal = ?";
     private final String SQL_DELETE = "delete from libur_nasional where tanggal like ?";
@@ -64,166 +71,182 @@ public class LiburNasionalDaoImplemen implements LiburNasionalDao{
     @Override
     public void update(LiburNasional liburNasional) throws SQLException {
         PreparedStatement statement = null;
-         try {
-             connection.setAutoCommit(false);
+        try {
+            connection.setAutoCommit(false);
 
-             statement = connection.prepareStatement(SQL_UPDATE);
-             statement.setString(1,liburNasional.getKeterangan());
-             statement.setString(2,liburNasional.getTanggal());
-             statement.executeUpdate();
+            statement = connection.prepareStatement(SQL_UPDATE);
+            statement.setString(1, liburNasional.getKeterangan());
+            statement.setString(2, liburNasional.getTanggal());
+            statement.executeUpdate();
 
-             connection.commit();
-         } catch (SQLException exception) {
-              connection.rollback();
-             throw exception;
-         } finally {
-             try {
-                 connection.setAutoCommit(true);
-                 if (statement != null) {
-                     statement.close();
-                 }
-             } catch (SQLException exception) {
-                 throw exception;
-             }
-         }
+            connection.commit();
+        } catch (SQLException exception) {
+            connection.rollback();
+            throw exception;
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException exception) {
+                throw exception;
+            }
+        }
     }
 
     @Override
     public void delete(LiburNasional unit) throws SQLException {
         PreparedStatement statement = null;
-         try {
-             connection.setAutoCommit(false);
+        try {
+            connection.setAutoCommit(false);
 
-             statement = connection.prepareStatement(SQL_DELETE);
-             statement.setString(1, unit.getTanggal());
-             statement.executeUpdate();
+            statement = connection.prepareStatement(SQL_DELETE);
+            statement.setString(1, unit.getTanggal());
+            statement.executeUpdate();
 
-             connection.commit();
-         } catch (SQLException exception) {
-              connection.rollback();
-             throw exception;
-         } finally {
-             try {
-                 connection.setAutoCommit(true);
-                 if (statement != null) {
-                     statement.close();
-                 }
-             } catch (SQLException exception) {
-                 throw exception;
-             }
-         }
+            connection.commit();
+        } catch (SQLException exception) {
+            connection.rollback();
+            throw exception;
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException exception) {
+                throw exception;
+            }
+        }
     }
 
     @Override
     public LiburNasional getByTanggalLibur(String tanggal) throws SQLException {
         PreparedStatement statement = null;
-         ResultSet result = null;
-         try {
-             connection.setAutoCommit(false);
-             statement = connection.prepareStatement(SQL_GETBYKODE);
+        ResultSet result = null;
+        try {
+            connection.setAutoCommit(false);
+            statement = connection.prepareStatement(SQL_GETBYKODE);
 
-               statement.setString(1, tanggal);
-             result = statement.executeQuery();
-             LiburNasional liburNasional = null;
-             if (result.next()) {
-                 liburNasional = new LiburNasional();
-                 liburNasional.setTanggal(result.getString("tanggal"));
-                 liburNasional.setKeterangan(result.getString("keterangan"));
-             }
+            statement.setString(1, tanggal);
+            result = statement.executeQuery();
+            LiburNasional liburNasional = null;
+            if (result.next()) {
+                liburNasional = new LiburNasional();
+                liburNasional.setTanggal(result.getString("tanggal"));
+                liburNasional.setKeterangan(result.getString("keterangan"));
+            }
 
-             connection.commit();
-             return liburNasional;
-         } catch (SQLException exception) {
-              connection.rollback();
-             throw exception;
-         } finally {
-             try {
-                 connection.setAutoCommit(true);
-                 if (result != null) {
-                     result.close();
-                 }
-                 if (statement != null) {
-                     statement.close();
-                 }
-             } catch (SQLException exception) {
-                 throw exception;
-             }
-         }
+            connection.commit();
+            return liburNasional;
+        } catch (SQLException exception) {
+            connection.rollback();
+            throw exception;
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                if (result != null) {
+                    result.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException exception) {
+                throw exception;
+            }
+        }
     }
 
     @Override
     public LiburNasional getByKeteranganLibur(String keterangan) throws SQLException {
         PreparedStatement statement = null;
-         ResultSet result = null;
-         try {
-             connection.setAutoCommit(false);
-             statement = connection.prepareStatement(SQL_GETBYNAMA);
-            
-               statement.setString(1, keterangan);
-               System.out.println(SQL_GETBYNAMA);
-             result = statement.executeQuery();
-             LiburNasional liburNasional = null;
-             if (result.next()) {
-                 liburNasional = new LiburNasional();
-                 liburNasional.setTanggal(result.getString("tanggal"));
-                 liburNasional.setKeterangan(result.getString("keterangan"));
-             }
+        ResultSet result = null;
+        try {
+            connection.setAutoCommit(false);
+            statement = connection.prepareStatement(SQL_GETBYNAMA);
 
-             connection.commit();
-             return liburNasional;
-         } catch (SQLException exception) {
-              connection.rollback();
-             throw exception;
-         } finally {
-             try {
-                 connection.setAutoCommit(true);
-                 if (result != null) {
-                     result.close();
-                 }
-                 if (statement != null) {
-                     statement.close();
-                 }
-             } catch (SQLException exception) {
-                 throw exception;
-             }
-         }
+            statement.setString(1, keterangan);
+            System.out.println(SQL_GETBYNAMA);
+            result = statement.executeQuery();
+            LiburNasional liburNasional = null;
+            if (result.next()) {
+                liburNasional = new LiburNasional();
+                liburNasional.setTanggal(result.getString("tanggal"));
+                liburNasional.setKeterangan(result.getString("keterangan"));
+            }
+
+            connection.commit();
+            return liburNasional;
+        } catch (SQLException exception) {
+            connection.rollback();
+            throw exception;
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                if (result != null) {
+                    result.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException exception) {
+                throw exception;
+            }
+        }
     }
 
     @Override
     public List<LiburNasional> getAllLiburNasional() throws SQLException {
         PreparedStatement statement = null;
-         ResultSet result = null;
-         try {
-             connection.setAutoCommit(false);
+        ResultSet result = null;
+        try {
+            connection.setAutoCommit(false);
 
-             statement = connection.prepareStatement(SQL_GETALL);
+            statement = connection.prepareStatement(SQL_GETALL);
 
-             result = statement.executeQuery();
-             List<LiburNasional> liburNasionals = new ArrayList<LiburNasional>();
-             while (result.next()) {
-                 LiburNasional liburNasional = new LiburNasional();
-                 liburNasional.setTanggal(result.getString("tanggal"));
-                 liburNasional.setKeterangan(result.getString("keterangan"));
-                 liburNasionals.add(liburNasional);
-             }
+            result = statement.executeQuery();
+            List<LiburNasional> liburNasionals = new ArrayList<LiburNasional>();
+            while (result.next()) {
 
-             connection.commit();
-             return liburNasionals;
-         } catch (SQLException exception) {
-             throw exception;
-         } finally {
-             try {
-                 connection.setAutoCommit(true);
-                 if (result != null) {
-                     result.close();
-                 }
-                 if (statement != null) {
-                     statement.close();
-                 }
-             } catch (SQLException exception) {
-                 throw exception;
+                SimpleDateFormat sdf = new SimpleDateFormat("dd-MM-yyyy");
+                String[] tanggalArr = new String[3];
+                int counter = 0;
+                StringTokenizer stoken = new StringTokenizer(result.getString("tanggal"));
+                while (stoken.hasMoreElements()) {
+
+                    tanggalArr[counter] = stoken.nextToken("-");
+                    counter++;
+                }
+                String tanggalGabung = tanggalArr[2] + "-" + tanggalArr[1] + "-" + tanggalArr[0];
+                Date tanggal = null;
+                try {
+                    tanggal = sdf.parse(tanggalGabung);
+                } catch (ParseException ex) {
+                    Logger.getLogger(LiburPerusahaanForm.class.getName()).log(Level.SEVERE, null, ex);
+                }
+                LiburNasional liburNasional = new LiburNasional();
+                liburNasional.setTanggal(sdf.format(tanggal));
+                liburNasional.setKeterangan(result.getString("keterangan"));
+                liburNasionals.add(liburNasional);
             }
-         }
+
+            connection.commit();
+            return liburNasionals;
+        } catch (SQLException exception) {
+            throw exception;
+        } finally {
+            try {
+                connection.setAutoCommit(true);
+                if (result != null) {
+                    result.close();
+                }
+                if (statement != null) {
+                    statement.close();
+                }
+            } catch (SQLException exception) {
+                throw exception;
+            }
+        }
     }
-    
 }
