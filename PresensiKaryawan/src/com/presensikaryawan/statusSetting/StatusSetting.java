88993e8172167ef9603206b567eeb406c43f4401
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-09-05 04:14:02 +0000

    Status Setting (start)

---
+++ PresensiKaryawan/src/com/presensikaryawan/statusSetting/StatusSetting.java
@@ -0,0 +1,46 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.presensikaryawan.statusSetting;
+
+/**
+ *
+ * @author Tinus
+ */
+public class StatusSetting {
+    private String kodeStatus;
+    private String keteranganStatus;
+
+    public StatusSetting() {
+    }
+
+    /**
+     * @return the kodeStatus
+     */
+    public String getKodeStatus() {
+        return kodeStatus;
+    }
+
+    /**
+     * @param kodeStatus the kodeStatus to set
+     */
+    public void setKodeStatus(String kodeStatus) {
+        this.kodeStatus = kodeStatus;
+    }
+
+    /**
+     * @return the keteranganStatus
+     */
+    public String getKeteranganStatus() {
+        return keteranganStatus;
+    }
+
+    /**
+     * @param keteranganStatus the keteranganStatus to set
+     */
+    public void setKeteranganStatus(String keteranganStatus) {
+        this.keteranganStatus = keteranganStatus;
+    }
+    
+}
