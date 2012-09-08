e274eb802a4533ec0b5e5618be59d3987eebad32
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-09-08 13:50:12 +0000

    Posisi

---
+++ trunk/PresensiKaryawan/src/com/presensikaryawan/posisi/Posisi.java
@@ -0,0 +1,47 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.presensikaryawan.posisi;
+
+/**
+ *
+ * @author Tinus
+ */
+public class Posisi {
+    private String kode_posisi;
+    private String nama_posisi;
+
+    public Posisi() {
+    }
+
+    /**
+     * @return the kode_posisi
+     */
+    public String getKode_posisi() {
+        return kode_posisi;
+    }
+
+    /**
+     * @param kode_posisi the kode_posisi to set
+     */
+    public void setKode_posisi(String kode_posisi) {
+        this.kode_posisi = kode_posisi;
+    }
+
+    /**
+     * @return the nama_posisi
+     */
+    public String getNama_posisi() {
+        return nama_posisi;
+    }
+
+    /**
+     * @param nama_posisi the nama_posisi to set
+     */
+    public void setNama_posisi(String nama_posisi) {
+        this.nama_posisi = nama_posisi;
+    }
+    
+    
+}
