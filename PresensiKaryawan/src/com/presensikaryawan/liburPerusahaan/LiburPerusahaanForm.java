a04f3030ddba7e64e4e2207d44c4da01433635f6
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-09-13 15:04:36 +0000

    heluussss....

--- PresensiKaryawan/src/com/presensikaryawan/liburPerusahaan/LiburPerusahaanForm.java
+++ PresensiKaryawan/src/com/presensikaryawan/liburPerusahaan/LiburPerusahaanForm.java
@@ -359,6 +359,29 @@ public class LiburPerusahaanForm extends javax.swing.JFrame {
             Logger.getLogger(LiburPerusahaanForm.class.getName()).log(Level.SEVERE, null, ex);
         }
         tanggalDateChooser.setDate(tanggal);
+//        System.out.println(tanggal.getDay());
+//        switch (tanggal.getDay()){
+//            case 0:
+//                System.out.println("Sunday");
+//                break;
+//            case 1:
+//                System.out.println("Monday");
+//                break;
+//            case 2:
+//                System.out.println("Tuesday");
+//                break;
+//            case 3:
+//                System.out.println("Wednesday");
+//                break;
+//            case 4:
+//                System.out.println("Thursday");
+//                break;
+//            case 5:
+//                System.out.println("Friday");
+//            case 6:
+//                System.out.println("Saturday");
+//                break;
+//        }
     }//GEN-LAST:event_liburPerusahaanTableMouseClicked
     private void isitable() {
     }
