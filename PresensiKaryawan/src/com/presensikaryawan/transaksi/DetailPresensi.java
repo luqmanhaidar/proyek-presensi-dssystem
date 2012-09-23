78c27c351f1e1495cee46a8996dfcc6c853816e3
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-09-23 08:19:13 +0000

    report

---
+++ PresensiKaryawan/src/com/presensikaryawan/transaksi/DetailPresensi.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.presensikaryawan.transaksi;
+
+import com.presensikaryawan.karyawan.Karyawan;
+
+/**
+ *
+ * @author Tinus
+ */
+public class DetailPresensi {
+    
+
+    public DetailPresensi() {
+    }
+
+
+}
