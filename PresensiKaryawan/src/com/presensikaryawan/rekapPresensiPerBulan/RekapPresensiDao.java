ab0f74ac823ce41189a12c66a7e7531594690e92
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-09-24 10:39:28 +0000

    rekap presensi per bulan

---
+++ PresensiKaryawan/src/com/presensikaryawan/rekapPresensiPerBulan/RekapPresensiDao.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.presensikaryawan.rekapPresensiPerBulan;
+
+import com.presensikaryawan.rekapPresensi.*;
+import com.presensikaryawan.departmentSetting.Department;
+import com.presensikaryawan.karyawan.Karyawan;
+import java.sql.SQLException;
+import java.util.List;
+
+/**
+ *
+ * @author Tinus
+ */
+public interface RekapPresensiDao {
+    List<Karyawan> getAllKaryawanByDepartmentCode(String code) throws SQLException;
+    List<Department> getAllDepartments() throws SQLException;
+    Department getNamaDepartmentByCode(String code) throws SQLException;
+    void callInsertAlfa(String maxTanggal, String nip) throws SQLException;
+    List<DetailPresensi> callGetPresensi(String bulan, String tahun, String kode_department) throws SQLException;
+}
