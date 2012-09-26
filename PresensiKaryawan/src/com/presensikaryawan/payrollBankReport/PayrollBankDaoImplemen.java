45af152b47264381e42b8a9a770aa5c4cb74f4de
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-09-26 16:43:30 +0000

    payroll bank

---
+++ PresensiKaryawan/src/com/presensikaryawan/payrollBankReport/PayrollBankDaoImplemen.java
@@ -0,0 +1,15 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.presensikaryawan.payrollBankReport;
+
+import java.sql.Connection;
+
+/**
+ *
+ * @author Tinus
+ */
+public class PayrollBankDaoImplemen implements PayrollBankDao{
+    private Connection connection;
+}
