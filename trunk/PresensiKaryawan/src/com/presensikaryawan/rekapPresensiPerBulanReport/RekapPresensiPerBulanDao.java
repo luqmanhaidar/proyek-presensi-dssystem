5434835060a0e06d235f79d4b53b8510d5877a98
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-09-26 13:35:16 +0000

    reportRekapPresensiPerBulan

--- trunk/PresensiKaryawan/src/com/presensikaryawan/rekapPresensiPerBulanReport/RekapPresensiPerBulanDao.java
+++ trunk/PresensiKaryawan/src/com/presensikaryawan/rekapPresensiPerBulanReport/RekapPresensiPerBulanDao.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.presensikaryawan.rekapPresensiPerBulan;
+package com.presensikaryawan.rekapPresensiPerBulanReport;
 
 import com.presensikaryawan.karyawan.Karyawan;
 import java.sql.SQLException;
--- trunk/PresensiKaryawan/src/com/presensikaryawan/rekapPresensiPerBulanReport/RekapPresensiPerBulanTableModel.java
+++ trunk/PresensiKaryawan/src/com/presensikaryawan/rekapPresensiPerBulanReport/RekapPresensiPerBulanTableModel.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.presensikaryawan.rekapPresensiPerBulan;
+package com.presensikaryawan.rekapPresensiPerBulanReport;
 
 import com.presensikaryawan.rekapPresensi.*;
 import java.util.ArrayList;
