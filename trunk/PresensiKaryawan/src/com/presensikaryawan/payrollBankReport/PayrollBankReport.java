b5bc6a8bdb8eab58a14233f5a66f6ff2a901752f
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-09-26 16:43:30 +0000

    payroll bank

---
+++ trunk/PresensiKaryawan/src/com/presensikaryawan/payrollBankReport/PayrollBankReport.java
@@ -0,0 +1,108 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.presensikaryawan.payrollBankReport;
+
+/**
+ *
+ * @author Tinus
+ */
+public class PayrollBankReport {
+    private String noRekening;
+    private String nama;
+    private String bulanTahun;
+    private double jumlahGaji;
+    private double totalKeseluruhanGaji;
+    private int no;
+
+    public PayrollBankReport() {
+    }
+
+    /**
+     * @return the noRekening
+     */
+    public String getNoRekening() {
+        return noRekening;
+    }
+
+    /**
+     * @param noRekening the noRekening to set
+     */
+    public void setNoRekening(String noRekening) {
+        this.noRekening = noRekening;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the bulanTahun
+     */
+    public String getBulanTahun() {
+        return bulanTahun;
+    }
+
+    /**
+     * @param bulanTahun the bulanTahun to set
+     */
+    public void setBulanTahun(String bulanTahun) {
+        this.bulanTahun = bulanTahun;
+    }
+
+    /**
+     * @return the jumlahGaji
+     */
+    public double getJumlahGaji() {
+        return jumlahGaji;
+    }
+
+    /**
+     * @param jumlahGaji the jumlahGaji to set
+     */
+    public void setJumlahGaji(double jumlahGaji) {
+        this.jumlahGaji = jumlahGaji;
+    }
+
+    /**
+     * @return the totalKeseluruhanGaji
+     */
+    public double getTotalKeseluruhanGaji() {
+        return totalKeseluruhanGaji;
+    }
+
+    /**
+     * @param totalKeseluruhanGaji the totalKeseluruhanGaji to set
+     */
+    public void setTotalKeseluruhanGaji(double totalKeseluruhanGaji) {
+        this.totalKeseluruhanGaji = totalKeseluruhanGaji;
+    }
+
+    /**
+     * @return the no
+     */
+    public int getNo() {
+        return no;
+    }
+
+    /**
+     * @param no the no to set
+     */
+    public void setNo(int no) {
+        this.no = no;
+    }
+    
+    
+    
+}
