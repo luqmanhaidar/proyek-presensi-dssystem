a405d64fbc22e5dfb4423381a14c3241bc5514c9
agustinuswikrama <agustinuswikrama@50444160-455f-c419-77db-0478cce327b8>
2012-10-03 14:20:34 +0000

    commit

---
+++ PresensiKaryawan/src/com/presensikaryawan/shiftReport/ShiftReport.java
@@ -0,0 +1,73 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.presensikaryawan.shiftReport;
+
+/**
+ *
+ * @author Tinus
+ */
+public class ShiftReport {
+    private String nip, tanggal, nama, kode_shift;
+
+    public ShiftReport() {
+    }
+
+    /**
+     * @return the nip
+     */
+    public String getNip() {
+        return nip;
+    }
+
+    /**
+     * @param nip the nip to set
+     */
+    public void setNip(String nip) {
+        this.nip = nip;
+    }
+
+    /**
+     * @return the tanggal
+     */
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    /**
+     * @param tanggal the tanggal to set
+     */
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    /**
+     * @return the nama
+     */
+    public String getNama() {
+        return nama;
+    }
+
+    /**
+     * @param nama the nama to set
+     */
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    /**
+     * @return the kode_shift
+     */
+    public String getKode_shift() {
+        return kode_shift;
+    }
+
+    /**
+     * @param kode_shift the kode_shift to set
+     */
+    public void setKode_shift(String kode_shift) {
+        this.kode_shift = kode_shift;
+    }
+    
+}
---
+++ PresensiKaryawan/src/com/presensikaryawan/shiftReport/ShiftReportDao.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.presensikaryawan.shiftReport;
+
+/**
+ *
+ * @author Tinus
+ */
+public interface ShiftReportDao {
+    
+}
